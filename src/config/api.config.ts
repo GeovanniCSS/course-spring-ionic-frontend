760670a459cc3a3520feafeef403161ab25a77f6
Geovanni Corsino <geovannicorsino@gmail.com>
2021-01-21 08:54:50 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://course-spring-ionic-geovanni.s3-sa-east-1.amazonaws.com"
 }
